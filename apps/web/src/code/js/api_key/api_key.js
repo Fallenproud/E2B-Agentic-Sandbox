f3b7b7e4505087691615384635969069be6c95ab
Fallenproud <5671859+Fallenproud@users.noreply.github.com>
2025-08-02 10:10:51 +0200

    use direct API key in JS example

--- apps/web/src/code/js/api_key/api_key.js
+++ apps/web/src/code/js/api_key/api_key.js
@@ -1,5 +1,7 @@
-import 'dotenv/config'
 import { Sandbox } from '@e2b/code-interpreter'
 
-const sandbox = await Sandbox.create()
+const sandbox = await Sandbox.create({
+  apiKey: 'e2b_ef21b788cc7db44fa69ed165aa54617cc3ed1dc5',
+})
+
 await sandbox.close()
